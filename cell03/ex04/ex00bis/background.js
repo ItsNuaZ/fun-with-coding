06e50e1fd28f48a5047ba646016f7fa9353d1a63
Nua <44229262+ItsNuaZ@users.noreply.github.com>
2025-09-10 17:50:54 +0000

    feat: complete ex00bis - js button but remade in jquery

---
+++ cell03/ex04/ex00bis/background.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    function getRandom(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    function randomizeRGB() {
+        let r = getRandom(0, 255);
+        let g = getRandom(0, 255);
+        let b = getRandom(0, 255);
+
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+
+    $(".button").click(function(){
+        $("body").css("background", randomizeRGB());
+    })
+});
